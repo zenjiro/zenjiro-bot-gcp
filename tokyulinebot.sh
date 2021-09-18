4f66094f989edaa67cd197a4fb142a643d8e1b70
zenjiro <zenjiro0123@gmail.com>
2021-09-19 06:33:58 +0900

    東急電鉄なうのシェルスクリプト版を登録

---
+++ tokyulinebot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -u
+while true
+do
+    curl -s -S https://www.tokyu.co.jp/unten/unten2.json | jq -r .unten > .tokyulinebot-now
+    [ -f .tokyulinebot-last ] && diff .tokyulinebot-now .tokyulinebot-last > /dev/null || (cat .tokyulinebot-now && ~/tokyulinebot/tweet.sh post < .tokyulinebot-now)
+    mv .tokyulinebot-now .tokyulinebot-last
+    sleep 60
+done
