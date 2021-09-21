77424398c8d9c963199d8e9d8fc55bfbf9c31011
zenjiro <zenjiro0123@gmail.com>
2021-09-21 15:19:02 +0900

    <br>を改行に置換

--- tokyulinebot.sh
+++ tokyulinebot.sh
@@ -2,7 +2,7 @@
 set -u
 while true
 do
-    curl -s -S https://www.tokyu.co.jp/unten/unten2.json | jq -r .unten > .tokyulinebot-now
+    curl -s -S https://www.tokyu.co.jp/unten/unten2.json | jq -r .unten | sed "s/<br>/\n/g" > .tokyulinebot-now
     [ -f .tokyulinebot-last ] && diff .tokyulinebot-now .tokyulinebot-last > /dev/null || (cat .tokyulinebot-now && ~/tokyulinebot/tweet.sh post < .tokyulinebot-now)
     mv .tokyulinebot-now .tokyulinebot-last
     sleep 60
