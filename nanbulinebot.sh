4211d4450be72783bf59d2be9ad60d8b7331146e
zenjiro <zenjiro0123@gmail.com>
2021-10-12 22:31:12 +0900

    平常運転のときにも投稿

--- nanbulinebot.sh
+++ nanbulinebot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -u
+set -u -o pipefail
 while true
 do
-    curl -s -S --location http://traininfo.jreast.co.jp/train_info/kanto.aspx | grep "<p class=\"mt10 sp_mt5 sp_mr25\">南武線" | sed -r -e "s|<[^>]+>||g" -e "s/&nbsp;//g" -e "s/[	 ^M]//g" -e "s/\r//g" > .nanbulinebot-now
+    curl -s -S --location http://traininfo.jreast.co.jp/train_info/kanto.aspx | grep "<p class=\"mt10 sp_mt5 sp_mr25\">南武線" | sed -r -e "s|<[^>]+>||g" -e "s/&nbsp;//g" -e "s/[	 ^M]//g" -e "s/\r//g" > .nanbulinebot-now || echo 南武線は、現在平常運転しています。 > .nanbulinebot-now
     [ -f .nanbulinebot-last ] && diff .nanbulinebot-now .nanbulinebot-last > /dev/null || (cat .nanbulinebot-now && ~/nanbulinebot/tweet.sh post < .nanbulinebot-now)
     mv .nanbulinebot-now .nanbulinebot-last
     sleep 60
